004041929114035cb64bbb970367b418711b5944
AShi <aunshilord@yahoo.com>
2015-01-17 12:15:10 +0300

    Project created

---
+++ MyDictionary/ViewControllerSearch.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerSearch.h
+//  MyDictionary
+//
+//  Created by robert on 1/17/15.
+//  Copyright (c) 2015 ashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerSearch : UIViewController
+
+@end
---
+++ MyDictionary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyDictionary
+//
+//  Created by robert on 1/17/15.
+//  Copyright (c) 2015 ashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
