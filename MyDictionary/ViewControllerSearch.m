4981f3d63eb2aadfd4629fffa00e6e7da44aa7f5
AShi <aunshilord@yahoo.com>
2015-01-17 14:47:00 +0300

    Getting html from API -Добавлено немного интерфейса на форму поиска -Добавлен метод Search, который забирает из API и выводит в TextView

--- MyDictionary/ViewControllerSearch.m
+++ MyDictionary/ViewControllerSearch.m
@@ -10,6 +10,10 @@
 
 @interface ViewControllerSearch ()
 
+@property (strong, nonatomic) IBOutlet UITextField *textField;
+@property (strong, nonatomic) IBOutlet UIButton *searchButton;
+@property (strong, nonatomic) IBOutlet UITextView *textView;
+
 @end
 
 @implementation ViewControllerSearch
@@ -22,6 +26,16 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//- (IBAction)Search:(id)sender {
+- (IBAction)Search {
+    // NSURL *url12 = [NSURL URLWithString: @"http://bbs.sjtu.edu.cn/file/bbs/mobile/top100.html"];
+    NSURL *url12 = [NSURL URLWithString: [@"https://dictionary.yandex.net/api/v1/dicservice/lookup?key=dict.1.1.20150105T101949Z.bc712af51ea9580c.bd4702cc00d35c7a895636ff917b517502a61c5d&lang=en-en&text=" stringByAppendingString: self.textField.text]];
+    NSString *res = [NSString stringWithContentsOfURL: url12
+                                             encoding: CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingGB_18030_2000) error: nil];
+    NSLog(@"%@", res);
+    self.textView.text = res;
 }
 
 /*
