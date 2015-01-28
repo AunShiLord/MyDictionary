c50ceeb8ef2b9224eb84484485274ba95c01a978
AShi <aunshilord@yahoo.com>
2015-01-28 10:35:39 +0300

    Core data attached to project

--- MyDictionary/AppDelegate.h
+++ MyDictionary/AppDelegate.h
@@ -7,11 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ViewControllerSearch.h"
+#import "ViewControllerDictionary.h"
+#import "ViewControllerTags.h"
+#import <CoreData/CoreData.h>
+#import <Foundation/Foundation.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
+// Core Data
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void) saveContext;
+- (NSURL *) applicationsDocumentsDirectory;
 
 @end
 
--- MyDictionary/AppDelegate.m
+++ MyDictionary/AppDelegate.m
@@ -7,9 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewControllerSearch.h"
-#import "ViewControllerDictionary.h"
-#import "ViewControllerTags.h"
+
 
 @interface AppDelegate ()
 
@@ -17,6 +15,9 @@
 
 @implementation AppDelegate
 
+@synthesize managedObjectContext = _managedObjectContext;
+@synthesize managedObjectModel = _managedObjectModel;
+@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
@@ -63,6 +64,66 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+// Core Data
+-(NSManagedObjectModel *) managedObjectModel {
+    if (_managedObjectModel != nil)
+    {
+        return _managedObjectModel;
+    }
+    
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource: @"DMDictionaryWord" withExtension: @"momd"];
+    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL: modelURL];
+    
+    return _managedObjectModel;
+}
+
+-(NSPersistentStoreCoordinator *)persistentStoreCoordinator{
+    if (_persistentStoreCoordinator != nil) {
+        return _persistentStoreCoordinator;
+    }
+    
+    NSURL *storeURL = [[self applicationsDocumentsDirectory] URLByAppendingPathComponent: @"MyDictionary.sqlite"];
+    
+    NSError *error = nil;
+    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+    if (![_persistentStoreCoordinator addPersistentStoreWithType: NSSQLiteStoreType configuration: nil URL: storeURL options: nil error: &error]) {
+        NSLog(@"Error acquired %@, %@", error, [error userInfo]);
+        abort();
+    }
+    
+    return _persistentStoreCoordinator;
+}
+
+-(NSManagedObjectContext *) managedObjectContext {
+    if(_managedObjectContext != nil) {
+        return _managedObjectContext;
+    }
+    
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil) {
+        _managedObjectContext = [[NSManagedObjectContext alloc] init];
+        [_managedObjectContext setPersistentStoreCoordinator: coordinator];
+    }
+    
+    return _managedObjectContext;
+}
+
+- (NSURL *) applicationsDocumentsDirectory {
+    return  [[[NSFileManager defaultManager] URLsForDirectory: NSDocumentDirectory inDomains: NSUserDomainMask] lastObject];
+}
+
+- (void)saveContext {
+    NSError *error = nil;
+    NSManagedObjectContext *managedObjectContext = [self managedObjectContext];
+    
+    if (managedObjectContext != nil) {
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save: &error] ) {
+            NSLog(@"Shit happened! %@, %@", error, [error userInfo]);
+            abort();
+        }
+    }
 }
 
 @end
